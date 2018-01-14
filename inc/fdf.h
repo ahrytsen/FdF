bd4f00b4573571be5093364426146ed72156d952
Artur HRYTSENKO <ahrytsen@e2r13p10.unit.ua>
2018-01-14 16:10:55 +0200

    fix names

---
+++ inc/fdf.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FdF.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/13 14:55:43 by ahrytsen          #+#    #+#             */
+/*   Updated: 2018/01/14 16:00:55 by ahrytsen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <mlx.h>
+# include <libft.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <stdio.h>
+# include <math.h>
+
+# define W_WIDTH 1000
+# define W_HEIGHT 800
+# define ESC 53
+# define RAD_X -60.0 * (M_PI / 180.0)
+# define RAD_Y 45.0 * (M_PI / 180.0)
+# define RAD_Z 0.0 * (M_PI / 180.0)
+# define SZ 20
+# define COLOR 0x00FFFFFF
+
+typedef struct	s_point
+{
+	double	x;
+	double	y;
+	double	z;
+}				t_point;
+
+typedef struct	s_env
+{
+	void	*mlx;
+	void	*win;
+	int		s_w;
+	int		s_h;
+	t_list	*data;
+}				t_env;
+
+t_point			ft_conv_point(t_point p, t_env *env);
+void			ft_draw_line(t_env *env, t_point p0, t_point p1);
+
+#endif
---
+++ src/fdf.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FdF.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/13 14:56:03 by ahrytsen          #+#    #+#             */
+/*   Updated: 2018/01/14 16:05:00 by ahrytsen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fdf.h>
+
+static int	ft_esc(int key)
+{
+	key == ESC ? exit(0) : 0;
+	return (0);
+}
+
+static int	ft_save_line(char *line, t_env *env)
+{
+	char	**spl;
+	int		*tmp;
+	int		i;
+
+	spl = ft_strsplit(line, ' ');
+	i = 0;
+	while (spl[i])
+		i++;
+	!(tmp = malloc(sizeof(int) * i)) ? perror("./fdf") : 0;
+	!tmp ? exit(1) : 0;
+	i = 0;
+	while (spl[i])
+	{
+		tmp[i] = ft_atoi(spl[i]);
+		free(spl[i++]);
+	}
+	if (!ft_lstpush_back(&env->data, tmp, sizeof(int) * i))
+	{
+		perror("./fdf");
+		exit(1);
+	}
+	free(tmp);
+	free(spl);
+	return (i);
+}
+
+static void	ft_read_map(int fd, t_env *env)
+{
+	char	*line;
+	int		ret;
+
+	(get_next_line(fd, &line) != 1)
+		? exit(write(2, "./fdf: map error\n", 17) > 0) : env->s_h++;
+	env->s_w = ft_save_line(line, env);
+	free(line);
+	while ((ret = get_next_line(fd, &line)))
+	{
+		(ft_save_line(line, env) != env->s_w || ret == -1)
+			? exit(write(2, "./fdf: map error\n", 17) > 0) : env->s_h++;
+		free(line);
+	}
+}
+
+static void	ft_print(t_env *env, t_list *data)
+{
+	int		x;
+	int		y;
+
+	y = -1;
+	while (data && ++y < env->s_h)
+	{
+		x = -1;
+		while (++x < env->s_w)
+		{
+			if (data->next)
+				ft_draw_line(env, ft_conv_point((t_point){x * SZ, y * SZ,
+								((int*)data->content)[x] * (SZ / 2)}, env),
+							ft_conv_point((t_point){x * SZ, (y + 1) * SZ,
+										((int*)data->next->content)[x]
+										* (SZ / 2)}, env));
+			if (x + 1 < env->s_w)
+				ft_draw_line(env, ft_conv_point((t_point){x * SZ, y * SZ,
+								((int*)data->content)[x] * (SZ / 2)}, env),
+							ft_conv_point((t_point){(x + 1) * SZ, y * SZ,
+										((int*)data->content)[x + 1]
+										* (SZ / 2)}, env));
+		}
+		data = data->next;
+	}
+}
+
+int			main(int ac, char **av)
+{
+	int				fd;
+	static t_env	env;
+
+	(ac != 2) ? exit(ft_printf("usage: ./fdf map\n") > 0) : 0;
+	((fd = open(av[1], O_RDONLY)) == -1) ? perror(av[0]) : 0;
+	fd == -1 ? exit(1) : 0;
+	ft_read_map(fd, &env);
+	env.mlx = mlx_init();
+	env.win = mlx_new_window(env.mlx, W_WIDTH, W_HEIGHT, "FdF by ahrytsen!");
+	ft_print(&env, env.data);
+	mlx_key_hook(env.win, &ft_esc, 0);
+	mlx_hook(env.win, 17, 0, (int (*)())&exit, 0);
+	mlx_loop(env.mlx);
+}
